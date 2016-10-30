467aec117819ad7d58256958943285daffae39a2
Jordy Moos <jordymoos@gmail.com>
2016-10-30 20:11:31 +0100

    Starting with elm

---
+++ api.js
@@ -0,0 +1,10 @@
+let jsonServer = require('json-server');
+
+let server = jsonServer.create();
+server.use(jsonServer.defaults());
+
+let router = jsonServer.router('db.json');
+server.use(router);
+
+console.log('Listening at 4000');
+server.listen(4000);
